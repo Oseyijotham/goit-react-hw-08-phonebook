a3d3fc0ce37459e746c7ff4bd0d43261ddeab355
Oseyijotham <61389893+Oseyijotham@users.noreply.github.com>
2024-05-24 16:27:14 +0000

    Deploying to gh-pages from @ Oseyijotham/goit-react-hw-08-phonebook@778518620629f91cf07938e028e70f9254c728c8 🚀

---
+++ static/js/461.9222e691.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[461],{8461:function(e,r,n){n.r(r),n.d(r,{SharedFooter:function(){return s},default:function(){return l}});var t=n(7689),a="SharedFooter_footer__-3K5k",o=n(2791),c=n(184),s=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(o.Suspense,{fallback:(0,c.jsx)("div",{children:"Loading page..."}),children:(0,c.jsx)(t.j3,{})}),(0,c.jsx)("div",{className:a,children:"\xa9 2024 - Yellow Connect"})]})},l=s}}]);
+//# sourceMappingURL=461.9222e691.chunk.js.map
